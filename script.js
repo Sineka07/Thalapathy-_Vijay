32bdcd1a081c015229963502961c0d9fbe8d991c
Sineka07 <snehasenthilkumar91@gmail.com>
2026-01-08 23:16:13 +0530

    Update the style.css

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-let music=false;
 function bgm() {
-
-    const song= document.getElementById("song");
+    const song = document.getElementById("song");
     song.volume = 0.6;
-    music=true;
-    song.play();
+    song.load();   // important
+    song.play().catch(err => {
+        console.log("Audio play error:", err);
+    });
 }
+//function leo() {
+  //  const thrisha = document.getElementById("thrisha");
+   // thrisha.volume = 0.6;
+   // thrisha.load();   // important
+   // thrisha.play().catch(err => {
+     //   console.log("Audio play error:", err);
+   // });
+//}
+
+function leo() {
+    const thrisha = document.getElementById("thrisha");
+
+    if (thrisha.paused) {
+        thrisha.volume = 0.6;
+        thrisha.play().catch(err => {
+            console.log("Audio play error:", err);
+        });
+    } else {
+        thrisha.pause();
+        thrisha.currentTime = 0; // optional: restart from beginning
+    }
+}
+
+
