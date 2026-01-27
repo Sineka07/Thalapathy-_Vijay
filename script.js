4d24304c45902554a6285cd02d219ba58c02d54e
Sineka07 <snehasenthilkumar91@gmail.com>
2026-01-27 20:28:27 +0530

    Update the title of hover

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
+
 function bgm() {
     const song = document.getElementById("song");
-    song.volume = 0.6;
-    song.load();   // important
-    song.play().catch(err => {
-        console.log("Audio play error:", err);
-    });
+
+    if (song.paused) {
+        song.volume = 0.6;
+        song.play().catch(err => {
+            console.log("Audio play error:", err);
+        });
+    } else {
+        song.pause();
+        song.currentTime = 0; // optional: restart from beginning
+    }
 }
-//function leo() {
-  //  const thrisha = document.getElementById("thrisha");
-   // thrisha.volume = 0.6;
-   // thrisha.load();   // important
-   // thrisha.play().catch(err => {
-     //   console.log("Audio play error:", err);
-   // });
-//}
 
 function leo() {
     const thrisha = document.getElementById("thrisha");
@@ -28,5 +26,119 @@ function leo() {
         thrisha.currentTime = 0; // optional: restart from beginning
     }
 }
+
+function tvks() {
+    const kodi = document.getElementById("kodi");
+
+    if (kodi.paused) {
+        kodi.volume = 0.6;
+        kodi.play().catch(err => {
+            console.log("Audio play error:", err);
+        });
+    } else {
+        kodi.pause();
+        kodi.currentTime = 0; // optional: restart from beginning
+    }
+}
+
+function varisu() {
+    const rasmi = document.getElementById("rasmi");
+
+    if (rasmi.paused) {
+        rasmi.volume = 0.6;
+        rasmi.play().catch(err => {
+            console.log("Audio play error:", err);
+        });
+    } else {
+        rasmi.pause();
+        rasmi.currentTime = 0; // optional: restart from beginning
+    }
+}
+
+function jilla() {
+    const sakthi = document.getElementById("sakthi");
+
+    if (sakthi.paused) {
+        sakthi.volume = 0.6;
+        sakthi .play().catch(err => {
+            console.log("Audio play error:", err);
+        });
+    } else {
+        sakthi.pause();
+        sakthi.currentTime = 0; // optional: restart from beginning
+    }
+}
+function jilla() {
+    const sakthi = document.getElementById("sakthi");
+
+    if (sakthi.paused) {
+        sakthi.volume = 0.6;
+        sakthi .play().catch(err => {
+            console.log("Audio play error:", err);
+        });
+    } else {
+        sakthi.pause();
+        sakthi.currentTime = 0; // optional: restart from beginning
+    }
+}
+function jilla() {
+    const sakthi = document.getElementById("sakthi");
+
+    if (sakthi.paused) {
+        sakthi.volume = 0.6;
+        sakthi .play().catch(err => {
+            console.log("Audio play error:", err);
+        });
+    } else {
+        sakthi.pause();
+        sakthi.currentTime = 0; // optional: restart from beginning
+    }
+}
+
+function sachuu() {
+    const manga = document.getElementById("manga");
+
+    if (manga.paused) {
+        manga.volume = 0.6;
+        manga.play().catch(err => {
+            console.log("Audio play error:", err);
+        });
+    } else {
+        manga.pause();
+        manga.currentTime = 0; // optional: restart from beginning
+    }
+}
+
+function cm() {
+    const pooja = document.getElementById("pooja");
+
+    if (pooja.paused) {
+        pooja.volume = 0.6;
+        pooja.play().catch(err => {
+            console.log("Audio play error:", err);
+        });
+    } else {
+        pooja.pause();
+        pooja.currentTime = 0; // optional: restart from beginning
+    }
+}
+function sarkar(){
+    const keerthi = document.getElementById("keerthi");
+
+    if (keerthi.paused) {
+        keerthi.volume = 0.6;
+        keerthi.play().catch(err => {
+            console.log("Audio play error:", err);
+        });
+    } else {
+        keerthi.pause();
+        keerthi.currentTime = 0; // optional: restart from beginning
+    }
+}
+
+
+
+
+
 
 
